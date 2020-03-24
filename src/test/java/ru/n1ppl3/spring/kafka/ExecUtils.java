c16343c839e679f7a63218b5951381722b1b03a1
n1ppl3 <n1ppl3@gmail.com>
2020-03-24 16:18:44 +0300

    ExecUtils.java & ReflectUtils.java

---
+++ src/test/java/ru/n1ppl3/spring/kafka/ExecUtils.java
@@ -0,0 +1,31 @@
+package ru.n1ppl3.spring.kafka;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+public abstract class ExecUtils {
+
+    public static <T> List<T> executeParallelTasks(List<Callable<T>> callableList) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newFixedThreadPool(callableList.size());
+        try {
+            return doWithExecutor(executorService, callableList);
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    private static <T> List<T> doWithExecutor(ExecutorService executorService, List<Callable<T>> callableList) throws InterruptedException, ExecutionException {
+        List<T> result = new ArrayList<>(callableList.size());
+        List<Future<T>> futures = executorService.invokeAll(callableList);
+        for (Future<T> future : futures) {
+            result.add(future.get());
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/n1ppl3/spring/kafka/ReflectUtils.java
@@ -0,0 +1,19 @@
+package ru.n1ppl3.spring.kafka;
+
+import org.springframework.lang.NonNull;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+
+
+public abstract class ReflectUtils {
+
+    public static Object getObjectFieldValue(@NonNull Object object, String fieldName) {
+        Field field = ReflectionUtils.findField(object.getClass(), fieldName);
+        if (field == null) {
+            throw new RuntimeException("couldn't find " + fieldName + " field in " + object.getClass());
+        }
+        ReflectionUtils.makeAccessible(field);
+        return ReflectionUtils.getField(field, object);
+    }
+}
